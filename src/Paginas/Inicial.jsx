2c391c3b87210f5933cd7652240f1b8048791998
AlysonG14 <alyson.bosch.senai14@gmail.com>
2025-10-17 16:43:29 -0300

    Upload File 17/10 feat: image

--- src/Paginas/Inicial.jsx
+++ src/Paginas/Inicial.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.jpg';
+import logo from '../assets/IDDigital.jpg';
 import { useNavigate } from 'react-router-dom';
 
 export function Inicial() {
@@ -7,7 +7,6 @@ export function Inicial() {
   return (
     <main className="inicial">
       <img src={logo} className="logo" alt="Logo DS GO"  />
-     
       <button onClick={() => navigate('/dsgo')} className='entrar'>
         Entrar
       </button>
